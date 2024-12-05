d6910eed8f30e6244a7e2159900c01283dd26128
uday871 <uday871@gmail.com>
2024-12-05 18:24:02 +0530

    maxxxxxai

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const genAI = new GoogleGenerativeAI(apiKey);
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: ['https://maxxxxai.netlify.app', 'http://localhost:5173'], 
+  origin: ['https://maxxxxxai.netlify.app', 'http://localhost:5173'], 
 }));
 
 
